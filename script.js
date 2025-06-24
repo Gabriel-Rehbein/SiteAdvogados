78f28f34a1ec8e4ba1b99ed614ffea966adda424
gabriel-rehbein <gabrielmr0812@gmail.com>
2025-06-24 13:24:44 -0300

    versao 1.0

---
+++ script.js
@@ -0,0 +1,56 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("Site do escritório de advocacia carregado!");
+  // Aqui você pode colocar interações JS futuras
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const imagem = document.getElementById('imagemEscritorio');
+  const imagens = [
+    'Image/Firma/predio.png',
+    'Image/Firma/porta.png',
+    'Image/Firma/pordentro.png'
+  ];
+  let index = 0;
+  let intervalo;
+
+  function mostrarImagem(i) {
+    imagem.style.opacity = 0;
+    setTimeout(() => {
+      imagem.src = imagens[i];
+      imagem.style.opacity = 1;
+    }, 200);
+  }
+
+  function proximaImagem() {
+    index = (index + 1) % imagens.length;
+    mostrarImagem(index);
+  }
+
+  function imagemAnterior() {
+    index = (index - 1 + imagens.length) % imagens.length;
+    mostrarImagem(index);
+  }
+
+  document.querySelector('.seta.direita').addEventListener('click', () => {
+    proximaImagem();
+    reiniciarAutoPlay();
+  });
+
+  document.querySelector('.seta.esquerda').addEventListener('click', () => {
+    imagemAnterior();
+    reiniciarAutoPlay();
+  });
+
+  function iniciarAutoPlay() {
+    intervalo = setInterval(proximaImagem, 3000);
+  }
+
+  function reiniciarAutoPlay() {
+    clearInterval(intervalo);
+    iniciarAutoPlay();
+  }
+
+  iniciarAutoPlay();
+});
